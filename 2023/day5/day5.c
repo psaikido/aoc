bddfb2934cef2b03d839407be8a9a8d34722613d
Hugh Carroll <hughie@carrollonline.co.uk>
2023-12-07 00:03:40 +0000

    Parse an array of seed numbers.

--- 2023/day5/day5.c
+++ 2023/day5/day5.c
@@ -6,18 +6,54 @@
 #include <ctype.h>
 
 FILE* getFile();
+int* parseSeeds(char* line, int* seedCount);
 
 
 int main() 
 {
 	FILE *fp = getFile();
 	char line[1024];
+	int* seeds;
+	int seedCount = 0;
+	int lineCount = 0;
 
 	while (fgets(line, sizeof(line), fp)) {
 		printf("%s", line);
+
+		lineCount++;
+
+		if (lineCount == 1) {
+			seeds = parseSeeds(line, &seedCount);
+		}
+	}
+
+	printf("seedCount %d\n", seedCount);
+	for (int i = 1; i <= seedCount; i++) {
+		printf("s%d %d\n", i, seeds[i]);
 	}
 
 	return 0;
+}
+
+
+int* parseSeeds(char* line, int* seedCount)
+{
+	char* token;
+	int count = 0;
+	int* seeds = calloc(1024, sizeof(int));
+	int* ptr;
+
+	while((token = strtok_r(line, " \n", &line)) != NULL) {
+		if (count > 0) {
+			seeds[count] = (int)strtol(token, NULL, 10);
+		}
+
+		count++;
+	}
+
+	*seedCount = count - 1;
+	ptr = seeds;
+	return ptr;
 }
 
 
@@ -25,7 +61,7 @@ FILE* getFile()
 {
 	char filename[100];
 	strcpy (filename, getenv("HOME"));
-	strcat (filename, "/code/aoc/2023/day5/input.txt");
+	strcat (filename, "/code/aoc/2023/day5/input1.txt");
 
 	FILE *fp = NULL;
 	
